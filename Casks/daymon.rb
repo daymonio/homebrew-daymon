dac9513180a8d12dc97f64f51a78aec40659fabc
Vasily <vasily.t@astrastudio.ru>
2026-02-15 00:25:33 -0500

    Initial cask formula for Daymon

---
+++ Casks/daymon.rb
@@ -0,0 +1,27 @@
+cask "daymon" do
+  version "0.1.0"
+  sha256 "PLACEHOLDER"
+
+  url "https://github.com/daymonio/daymon/releases/download/v#{version}/Daymon-#{version}.dmg"
+  name "Daymon"
+  desc "Open source autopilot for Claude - scheduled tasks, memory, background automation"
+  homepage "https://daymon.io"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :monterey"
+
+  app "Daymon.app"
+
+  uninstall quit: "io.daymon.app"
+
+  zap trash: [
+    "~/Library/Application Support/daymon",
+    "~/Library/Preferences/io.daymon.app.plist",
+    "~/Library/Saved Application State/io.daymon.app.savedState",
+  ]
+end
